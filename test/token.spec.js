5539757c84b1b8889288af6d5c204724cd85c756
Rastislav Budinsky <rastislavbudinsky@gmail.com>
2021-06-24 14:40:53 +0200

    Add tests for FITCOIN Token

---
+++ test/token.spec.js
@@ -0,0 +1,40 @@
+import { expect } from 'chai'
+import { tokens, ether, ETHER_ADDRESS, EVM_REVERT, wait } from './helpers'
+
+const Token = artifacts.require('./Token.sol')
+
+require('chai')
+    .use(require('chai-as-promised'))
+        .should()
+
+contract('Token', accounts => {
+    let token
+
+    beforeEach(async() => {
+        token = await Token.new(accounts[0], accounts[1], accounts[2]);
+    })
+
+    describe('Testing token contract...', () => {
+        let team = 45*10**5
+        let ico = 3*10**6
+        let community = 15*10**5
+
+        it('checking token name', async() => {
+            expect(await token.name()).to.be.eq('FIT coin')
+        })
+
+        it('checking token symbol', async() => {
+            expect(await token.symbol()).to.be.eq('FITCOIN')
+        })
+
+        it('checking token initial total supply', async() => {
+            expect((await token.totalSupply()).toString()).to.eq(web3.utils.toWei((team + ico + community).toString()))
+        })
+
+        it('checking initial team, ico and community account supply', async() => {
+            expect((await token.balanceOf(accounts[0])).toString()).to.be.eq(web3.utils.toWei(team.toString()))
+            expect((await token.balanceOf(accounts[1])).toString()).to.be.eq(web3.utils.toWei(ico.toString()))
+            expect((await token.balanceOf(accounts[2])).toString()).to.be.eq(web3.utils.toWei(community.toString()))
+        })
+    })
+})
